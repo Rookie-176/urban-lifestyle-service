f942ca95375a91cfe5cb7f71a8740e1fb6db2904
Rookie-176 <Rookie-176@users.noreply.github.com>
2026-01-17 16:56:47 +1100

    feat: include RedisIdWorker for global unique id

---
+++ src/main/java/com/roger/urbanlifestyle/utils/RedisIdWorker.java
@@ -0,0 +1,31 @@
+package com.roger.urbanlifestyle.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+
+    private static final long BEGIN_TIMESTAMP=1735689600;
+    private static final int COUNT_BITS = 32;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    public long nextId (String keyPrefix) {
+
+        LocalDateTime now = LocalDateTime.now();
+        long timestamp=now.toEpochSecond(ZoneOffset.UTC) - BEGIN_TIMESTAMP;
+
+        String yyyyMMdd = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + yyyyMMdd);
+
+        return timestamp << 32 | count ;
+    }
+
+}
--- src/test/java/com/roger/urbanlifestyle/UlsApplicationTest.java
+++ src/test/java/com/roger/urbanlifestyle/UlsApplicationTest.java
@@ -4,10 +4,14 @@ import com.roger.urbanlifestyle.entity.Shop;
 import com.roger.urbanlifestyle.service.impl.ShopServiceImpl;
 import com.roger.urbanlifestyle.utils.CacheClient;
 import com.roger.urbanlifestyle.utils.RedisConstants;
+import com.roger.urbanlifestyle.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
@@ -15,14 +19,37 @@ public class UlsApplicationTest {
 
     @Resource
     private CacheClient cacheClient;
-
     @Resource
     private ShopServiceImpl shopService;
+    @Resource
+    private RedisIdWorker redisIdWorker;
 
     @Test
     void testSaveShop () throws InterruptedException {
         Shop shop = shopService.getById(1L);
         cacheClient.setWithLogicalExpire(RedisConstants.CACHE_SHOP_KEY+1L, shop, 10L, TimeUnit.SECONDS);
+    }
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(500);
+    @Test
+    void testRedisId () throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(300);
+
+        Runnable runnable = () -> {
+            for (int i = 0; i < 100; i++) {
+                long order = redisIdWorker.nextId("order");
+                System.out.println(order);
+            }
+            countDownLatch.countDown();
+        };
+
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            executorService.execute(runnable);
+        }
+        countDownLatch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time=" + (end - begin));
     }
 
 }
